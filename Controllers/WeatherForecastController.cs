3131c45a81362eddae9caf478615f2191cb90781
prabhakar Jinkala <reddy.jinkala2022@gmail.com>
2022-06-17 12:52:47 -0500

    Changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,7 +26,8 @@ public class WeatherForecastController : ControllerBase
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
+        
         .ToArray();
-    }
+        }
+    }    
 }
